05a5d9136250761c2cc628de0190b80f895a28df
abhi-zero <abhi.0zero@gmail.com>
2025-02-24 22:48:36 +0530

    Add JavaScript functionality for tip selection and input handling

---
+++ js/tip_app.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const bill = document.getElementById("billValue");
+  const tip = document.getElementById("tipValue");
+  const people = document.getElementById("peopleValue");
+  const tipSection = document.querySelector(".cta-section");
+
+  getTipValue(tip, tipSection);
+  function getTipValue(tipValue, fixedTipSection) {
+    let tip;
+    if (tipValue.value == "") {
+        fixedTipSection.addEventListener("click", (e) => {
+            const clickedButton = e.target.closest(".cta");
+            if (clickedButton) {
+                const selectedValue = clickedButton.dataset.value;
+                tip = selectedValue;
+                console.log(tip);
+            //    callBack(tip)
+                // You can't "return" from the event listener, but you can handle the value here
+              }
+              return tip;
+          });
+        }
+    if(tipValue.value !== "0"){
+        tipValue.addEventListener('input', (event) => {
+            tip = event.target.value;
+            console.log(tip);
+            return tip;
+          }); 
+    }
+  }
+
+  // function calculateTip(billAmount, tipValue, NoOfPeple){
+
+  // }
+});
