a49aa3b8e0148deefc8463a5175b45061675881d
abhi-zero <abhi.0zero@gmail.com>
2025-02-26 21:56:35 +0530

    Refactor tip calculation logic to improve accuracy and clarity, adding event listeners for input changes and separating calculation functions for tip and total amount.

--- js/tip_app.js
+++ js/tip_app.js
@@ -1,31 +1,48 @@
 document.addEventListener("DOMContentLoaded", () => {
   const bill = document.getElementById("billValue");
-  const tip = document.getElementById("tipValue");
+  const tipValue = document.getElementById("tipValue");
   const people = document.getElementById("peopleValue");
   const tipSection = document.querySelector(".cta-section");
 
-  getTipValue(tip, tipSection);
-  function getTipValue(tipValue, fixedTipSection) {
-    let tip;
-    if (tipValue.value == "") {
-        fixedTipSection.addEventListener("click", (e) => {
+  let tipPercent = 0;
+ 
+
+        tipSection.addEventListener("click", (e) => {
             const clickedButton = e.target.closest(".cta");
             if (clickedButton) {
-                const selectedValue = clickedButton.dataset.value;
-                tip = selectedValue;
-                console.log(tip);
+                tipPercent = parseInt(clickedButton.dataset.value) / 100;
+                tipValue.value = "";
               }
-              return tip;
           });
-        }
-    if(tipValue.value !== "0"){
+
         tipValue.addEventListener('input', (event) => {
-            tip = event.target.value;
-            console.log(tip);
-            return tip;
+            tipPercent = parseInt(event.target.value) / 100;
+            if (isNaN(tipPercent)) tipPercent = 0;
           }); 
-    }
+
+  function calculateTip(billValue, peopleValue){
+      if(billValue > 0 && peopleValue > 0 && tipPercent > 0){
+        const tipAmount = (((billValue * tipPercent) / 100) / peopleValue).toFixed(2);
+        console.log(tipAmount);
+        return tipAmount;
+      }
   }
 
+  function calculateTotalAmount(billValue, peopleValue){
+    const tipAmount = calculateTip(billValue, peopleValue);
+    const totalAmount = (((billValue  / peopleValue)) + parseFloat(tipAmount));
+    console.log(totalAmount);
+    return totalAmount;
+  }
 
+  [bill, people].forEach((input) =>
+      input.addEventListener("input", () => {
+         // bill value changes in to cent for better accuracy
+  const billValue = (parseFloat(bill.value)) * 100;
+  console.log(billValue);
+  const peopleValue = parseInt(people.value);
+        calculateTip(billValue, peopleValue);
+        calculateTotalAmount(billValue, peopleValue);
+      })
+  );
 });
